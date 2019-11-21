a0f832f9ee02010e5ce3f5161fe2d8be3bbaec6a
Mandy Alford <mandyalford@gmail.com>
2019-11-21 12:15:45 -0700

    Add styling for transaction section, functionality for welcome close

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 
+var closeImg = document.querySelector("#close-message")
+closeImg.addEventListener("click", removeWelcome);
+
+function removeWelcome(event) {
+  if (event.target.classList.contains("close")){
+    console.log("I was clicked");
+   event.target.parentElement.remove()
+   }
+  }
