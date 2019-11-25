8dd001bd7529248e67867097b226215b9be666c3
Mandy Alford <mandyalford@gmail.com>
2019-11-24 17:19:05 -0700

    Add functionality to highlight nav icons when clicked

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 
-var closeImg = document.querySelector("#close-message")
+var closeImg = document.querySelector("#close-message");
+var navDashboard= document.querySelector("#nav-dashboard");
+var navTransactions= document.querySelector("#nav-transactions");
+var navAssets= document.querySelector("#nav-assets");
+var navIcons= document.querySelector(".navigation");
+
 closeImg.addEventListener("click", removeWelcome);
+navDashboard.addEventListener("click", activeNavItem);
+navTransactions.addEventListener("click", activeNavItem);
+navAssets.addEventListener("click", activeNavItem);
+
 
 function removeWelcome(event) {
   if (event.target.classList.contains("close")){
-    console.log("I was clicked");
    event.target.parentElement.remove()
    }
   }
+
+function activeNavItem(event){
+  if (event.target.classList.contains("navigation")){
+    console.log("I was clicked");
+    event.target.classList.toggle("active");
+  }
+}
