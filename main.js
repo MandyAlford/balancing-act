bda191b27514e3c767ab92bd9fb48149c65c364b
Mandy Alford <mandyalford@gmail.com>
2019-11-26 09:56:07 -0700

    Add functionality to remove dash content when wallet icon is clicked

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var navDashboard= document.querySelector("#nav-dashboard");
 var navTransactions= document.querySelector("#nav-transactions");
 var navAssets= document.querySelector("#nav-assets");
 var navIcons= document.querySelector(".navigation");
+var financialOverview= document.querySelector(".financial-overview");
+var transactionOverview= document.querySelector(".transaction-wrapper");
 
 closeImg.addEventListener("click", removeWelcome);
 navDashboard.addEventListener("click", activeNavItem);
@@ -20,19 +22,34 @@ function removeWelcome(event) {
 function activeNavItem(event){
   if (event.target.classList.contains("navigation")){
     event.target.classList.toggle("active");
-    deActivateNavItem(event);
+    deActivateNavItem();
   }
 }
 
-function deActivateNavItem(event){
+function deActivateNavItem(){
   if (event.target===navDashboard){
     navTransactions.classList.remove("active");
     navAssets.classList.remove("active");
+    removeDashboardView();
   } else if (event.target===navTransactions){
     navDashboard.classList.remove("active");
     navAssets.classList.remove("active");
+    removeDashboardView();
   } else if (event.target===navAssets){
     navDashboard.classList.remove("active");
     navTransactions.classList.remove("active");
+    removeDashboardView();
+  }
+}
+
+function removeDashboardView (event){
+  if (navTransactions.classList.contains("active")){
+    console.log("trans was clicked")
+    financialOverview.remove();
+    transactionOverview.remove();
+  } else if (navDashboard.classList.contains("active")){
+    console.log("dash was clicked")
+    financialOverview.add();
+    transactionOverview.add();
   }
 }
